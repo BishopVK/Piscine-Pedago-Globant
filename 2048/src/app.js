088cd55f3742d951fb1cf0bf478e2407e012ed72
Daniel Jimenez Graindorge <danjimen@c3r12s2.42madrid.com>
2025-12-01 18:44:42 +0100

    MoveUp done (whit bugs??)

--- 2048/src/app.js
+++ 2048/src/app.js
@@ -13,25 +13,29 @@ function initGame() {
     grid.appendChild(cell);
   }
 
-  captureKeysEvent();
   fillStartingCells();
 }
 
 // Ejecutar el juego al cargar la página
+captureKeysEvent();
+restartButton();
 initGame();
 
 function fillStartingCells() {
     let firstCell = selectRandomCell();
     let secondCell = selectRandomCell();
 
-    while (firstCell === secondCell)
+    while (firstCell === secondCell) {
       secondCell = selectRandomCell();
+    }
 
     let cell = document.querySelector("#cell" + firstCell);
     cell.innerHTML = `${generateRandomValue()}`;
+    cellColour(cell);
 
     cell = document.querySelector("#cell" + secondCell);
     cell.innerHTML = `${generateRandomValue()}`;
+    cellColour(cell);
 }
 
 function selectRandomCell() {
@@ -43,29 +47,133 @@ function generateRandomValue() {
   return num.toString();
 }
 
+function cellColour(cell) {
+  if (cell.innerHTML.trim() === "")
+    return;
+
+  cell.className = ''; // Eliminar los estilos actuales
+  if (cell.innerHTML ===  "2")
+    cell.classList.add("cell", "num2")
+  else if (cell.innerHTML ===  "4")
+    cell.classList.add("cell", "num4")
+}
+
 // Evento del botón restart
-const startButton = document.getElementById("restart-button");
-startButton.addEventListener("click", function () {
-  initGame();
-});
+function restartButton() {
+  const startButton = document.getElementById("restart-button");
+  startButton.addEventListener("click", function () {
+    initGame();
+  });
+}
 
 function captureKeysEvent() {
   document.addEventListener("keydown", (event) => {
     if (event.key === "w" || event.key === "ArrowUp") {
       event.preventDefault();
       console.log("Movimiento hacia arriba");
+      moveUp("up");
     }
     else if (event.key === "s" || event.key === "ArrowDown") {
       event.preventDefault();
       console.log("Movimiento hacia abajo");
+      moveUp("down");
     }
     else if (event.key === "a" || event.key === "ArrowLeft") {
       event.preventDefault();
       console.log("Movimiento hacia la izquierda");
+      moveUp("left");
     }
     else if (event.key === "d" || event.key === "ArrowRight") {
       event.preventDefault();
       console.log("Movimiento hacia la derecha");
+      moveUp("right");
     }
   });
+}
+
+function moveUp(move) {
+  // Vamos a repetir lo mismo columna por columna
+
+  // Guardamos los indices las celdas de cada columna
+  for (let i = 1; i <= 4; i++)
+  {
+    const colIdx = [i, i + 4, i + 8, i + 12];
+
+    // Vamos a recorrer las 4 celdas para guardar su contenido
+    let celsValues = [];
+    for (let j = 0; j < 4; j++) {
+      let cell = document.querySelector("#cell" + colIdx[j]);
+      celsValues.push(Number(cell.innerHTML));
+    }
+    console.log("celsValues:", celsValues); // DB
+    
+    // Al haber pulsado arriba la comprobación se hace de abajo a arriba, hay que voltear el array
+    celsValues = celsValues.reverse();
+    console.log("reversed:", celsValues);  // DB
+
+    // Crear array nuevo según la tecla pulsada
+    const newArray = createNewArray(celsValues);
+    console.log("newArray:", newArray); // DB
+
+    const finalArray = sumNumbers(newArray);
+    console.log("finalArray:", finalArray); // DB
+
+    for (let i = 0; i < 4; i++) {
+      let cell = document.querySelector("#cell" + colIdx[i]);
+      if (finalArray[i] === 0)
+        cell.innerHTML = "";
+      else 
+        cell.innerHTML = finalArray[i];
+
+    }
+  }
+}
+
+function createNewArray(celsValues) {
+  // Move al numbers to the first position
+  let newArray = [];
+  for (let i = 0; i < celsValues.length; i++) {
+    if (celsValues[i] !== 0) {
+      newArray.push(celsValues[i]);
+    }
+  }
+
+  console.log("newArray # elements:", newArray.length); // DB
+
+  // Fill whit 0
+  for (let filled = newArray.length; filled < 4; filled++) {
+    newArray.push(0);
+  }
+
+  return newArray;
+}
+
+function sumNumbers(newArray) {
+  let finalArray = [];
+
+  for (let i = 0; i < 4; i++) {
+    if (newArray[i] == 0)
+      continue;
+
+    let firstNbr = newArray[i];
+    let secondNbr = 0;
+    if (i < 3)
+      secondNbr = newArray[i + 1];
+
+    if (firstNbr === secondNbr) {
+      newArray[i + 1] = 0;
+      finalArray.push(firstNbr * 2);
+    }
+    else {
+      finalArray.push(firstNbr);
+      finalArray.push(secondNbr);
+    }
+  }
+
+  // Fill whit 0
+  for (let filled = finalArray.length; filled < 4; filled++) {
+    finalArray.push(0);
+  }
+
+  return finalArray;
 }
