43e0a5ba5364dcc33186d1b3dcda20c47a52b4f9
Daniel Jimenez Graindorge <danjimen@c3r12s2.42madrid.com>
2025-12-01 15:59:58 +0100

    Fixed CSS styles. 2 first numbers generated. KeyPressed events created

--- 2048/src/app.js
+++ 2048/src/app.js
@@ -6,42 +6,41 @@ function initGame() {
 
   for (let i = 1; i <= 16; i++) {
     const cell = document.createElement("div");
-    cell.id = `cell + ${i}`;
+    cell.id = `cell${i}`;
     cell.className = "cell";
     cell.innerHTML = ""; // Vacía la celda inicialmente
 
     grid.appendChild(cell);
   }
 
+  captureKeysEvent();
   fillStartingCells();
 }
 
 // Ejecutar el juego al cargar la página
 initGame();
 
-export function fillStartingCells() {
-  for (let i = 1; i <= 2; i++) {
-    let firstCell = Math.random() * 16 + 1;
+function fillStartingCells() {
+    let firstCell = selectRandomCell();
+    let secondCell = selectRandomCell();
 
-    while (firstCell === Math.random() * 16 + 1) {
-      let secondCell = Math.random() * 16 + 1;
-    }
+    while (firstCell === secondCell)
+      secondCell = selectRandomCell();
 
-    const cell = document.querySelector(`#cell${firstCell}`);
-    cell.innerHTML = `<h1>${generateRandomValue()}</h1>`;
-  }
-  while (firstCell === Math.random() * 16 + 1) {
-    secondCell = Math.random() * 16 + 1;
-  }
-  for (let i = 1; i <= 2; i++) {
-    const cell = document.querySelector(`#cell${i}`);
-    cell.innerHTML = firstCell;
-  }
+    let cell = document.querySelector("#cell" + firstCell);
+    cell.innerHTML = `${generateRandomValue()}`;
+
+    cell = document.querySelector("#cell" + secondCell);
+    cell.innerHTML = `${generateRandomValue()}`;
 }
 
-export function generateRandomValue() {
-  return Math.random() < 0.9 ? 2 : 4; // 90% de probabilidad de 2, 10% de 4
-  //cell.innerHTML = `<h1>${randomNumber}</h1>`;
+function selectRandomCell() {
+  return Math.floor(Math.random() * 16) + 1;
+}
+
+function generateRandomValue() {
+  const num = Math.random() < 0.9 ? 2 : 4; // 90% de probabilidad de 2, 10% de 4
+  return num.toString();
 }
 
 // Evento del botón restart
@@ -49,3 +48,24 @@ const startButton = document.getElementById("restart-button");
 startButton.addEventListener("click", function () {
   initGame();
 });
+
+function captureKeysEvent() {
+  document.addEventListener("keydown", (event) => {
+    if (event.key === "w" || event.key === "ArrowUp") {
+      event.preventDefault();
+      console.log("Movimiento hacia arriba");
+    }
+    else if (event.key === "s" || event.key === "ArrowDown") {
+      event.preventDefault();
+      console.log("Movimiento hacia abajo");
+    }
+    else if (event.key === "a" || event.key === "ArrowLeft") {
+      event.preventDefault();
+      console.log("Movimiento hacia la izquierda");
+    }
+    else if (event.key === "d" || event.key === "ArrowRight") {
+      event.preventDefault();
+      console.log("Movimiento hacia la derecha");
+    }
+  });
+}
