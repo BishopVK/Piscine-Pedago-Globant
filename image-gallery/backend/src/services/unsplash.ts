08ac7e655a85383cc1364311bffad13330143338
BishopVK <danielgraindorge@gmail.com>
2025-12-05 18:26:34 +0100

    Trying to fix TS bugs in the backend

--- image-gallery/backend/src/services/unsplash.ts
+++ image-gallery/backend/src/services/unsplash.ts
@@ -1,9 +1,11 @@
 const BASE_URL = "https://api.unsplash.com";
 
 export async function getPhotos(query?: string) {
+  const clientId = process.env.UNSPLASH_CLIENT_ID;
+  
   const url = query
-    ? `${BASE_URL}/search/photos?query=${query}&client_id=${import.meta.env.VITE_UNSPLASH_CLIENT_ID}`
-    : `${BASE_URL}/photos?client_id=${import.meta.env.VITE_UNSPLASH_CLIENT_ID}`;
+    ? `${BASE_URL}/search/photos?query=${query}&client_id=${clientId}`
+    : `${BASE_URL}/photos?client_id=${clientId}`;
 
   const res = await fetch(url);
   return res.json();
