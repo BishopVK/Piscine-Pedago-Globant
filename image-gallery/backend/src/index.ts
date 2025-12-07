e5922b8b05ad9a38007584f12720a51286c1065f
BishopVK <danielgraindorge@gmail.com>
2025-12-07 20:41:29 +0100

    Created error, 404, login and callback endpoints

--- image-gallery/backend/src/index.ts
+++ image-gallery/backend/src/index.ts
@@ -10,22 +10,46 @@ const PORT = process.env.APP_PORT || 3000;
 
 // Middlewares
 // Permite que el frontend (puerto 5173) llame al backend (puerto 3000).
-app.use(cors());
+//app.use(cors());
+app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }))
 
 // Permite recibir datos en formato JSON
 app.use(express.json());
 
+// Ruta de prueba
+app.get("/", (req: express.Request, res: express.Response) => {
+  res.status(200).send({
+    code: 200,
+    ok: true,
+    service: "Backend de gestión de fotos funcionando"
+  });
+});
+
 // Importar rutas
-import authRoutes from "./routes/auth.ts"; // Hay que poner la extensión .js auqnque el archivo sea .ts porque ts-node lo compila a .js
+import authRoutes from "./routes/auth.ts";
 import photoRoutes from "./routes/photos.ts";
 
 // Usar rutas
 app.use("/auth", authRoutes);
 app.use("/", photoRoutes);
 
-/* app.get("/", (req, res) => {
-  res.send("Conexión exitosa al backend");
-}); */
+// Manejo de rutas no encontradas
+app.use((req: express.Request, res: express.Response) => {
+  res.status(404).send({
+    code: 404,
+    ok: false,
+    message: "Ruta no encontrada" });
+});
+
+// Manejo de errores internos del servidor
+app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error("Error interno:", err);
+  res.status(500).send({
+    code: 500,
+    ok: false,
+    message: "Error interno del servidor",
+  });
+});
 
 // Arrancar servidor
 app.listen(PORT, () => {
--- image-gallery/backend/src/routes/auth.ts
+++ image-gallery/backend/src/routes/auth.ts
@@ -1,15 +1,70 @@
 import { Router } from "express";
+import dotenv from "dotenv";
+import axios from "axios";
+
+dotenv.config();
 
 const router = Router();
 
+// Unsplash Route
+const unsplashOauthUrl = "https://unsplash.com/oauth/authorize";
+const clientId = process.env.UNSPLASH_CLIENT_ID;
+const clientSecret = process.env.UNSPLASH_CLIENT_SECRET;
+const redirectUri = process.env.UNSPLASH_REDIRECT_URI;
+const scope = "public";
+
+if (!clientId || !clientSecret || !redirectUri) {
+  throw new Error("Faltan las variables de entorno de Unsplash.");
+}
+
 // GET /auth/login
 router.get("/login", (req, res) => {
-  res.send("Login endpoint (placeholder)");
+  console.log("Redirecting to Unsplash OAuth...");
+  res.redirect(`${unsplashOauthUrl}?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`);
 });
 
 // GET /auth/callback
-router.get("/callback", (req, res) => {
-  res.send("Callback endpoint (placeholder)");
+router.get("/callback", async (req, res) => {
+  try {
+    if (req.query.error) {
+      return res.status(400).send(`Error during authentication: ${req.query.error}`);
+    }
+
+    const authorizationCode = req.query.code as string;
+    if (!authorizationCode) {
+      return res.status(400).send("Authorization code not provided.");
+    }
+
+    // Código de autorización recibido
+    console.log("Received authorization code:", authorizationCode);
+    
+    // Intercambiar el código de autorización por un token de acceso
+    const unsplashTokenUrl = "https://unsplash.com/oauth/token";
+    const response = await axios.post(unsplashTokenUrl, {
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: redirectUri,
+      code: authorizationCode,
+      grant_type: "authorization_code",
+    }, {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const { access_token: accessToken } = response.data;
+    console.log("Received access Token:", accessToken);
+
+    // Guardar el token de acceso en una cookie temporal (solo desarrollo)S
+    res.cookie("access_token", accessToken, { httpOnly: true });
+
+    // Redirigir al frontend después del login exitoso
+    res.redirect(process.env.FRONTEND_URL || "http://localhost:5173");
+  } catch (error) {
+    console.error("Error during Unsplash OAuth callback:", error);
+    res.status(500).send("Internal Server Error");
+  }
+
 });
 
 export default router;
